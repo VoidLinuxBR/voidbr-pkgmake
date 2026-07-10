80f545cdb2d344259a82745f9c32ee6354f5dc9b
vcatafesta <vcatafesta@gmail.com>
2026-07-10 03:57:01 -0400

    sex 10 jul 2026 03:57:01 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- templates/hyprland/src/hyprland-source/hyprtester/src/tests/clients/tests.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <map>
-
-#include "../../shared.hpp"
-
-inline std::map<const char*, CTestCase&> clientTestCases;
-
-// Where `TEST_CASE` macros will store generated test cases:
-#define TEST_CASES_STORAGE clientTestCases
